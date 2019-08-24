e5cdd9500ac309b850c6bf11c1717d88605ab0a1
Anton Ryazanov <funfun2010@mail.com>
2019-08-24 22:40:18 +0300

    fix styled-components dep

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
 import {terser} from "rollup-plugin-terser";
 import babel from 'rollup-plugin-babel';
 import postcss from 'rollup-plugin-postcss';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 const config = {
     input: 'src/reactLoaderPlaceholder.js',
-    external: ['react', 'styled-components'],
+    external: ['react'],
     output: {
         format: 'umd',
         name: 'react-loader-placeholder',
@@ -14,9 +16,13 @@ const config = {
         }
     },
     plugins: [
+        resolve({preferBuiltins: true}),
         babel({exclude: "node_modules/**"}),
+        commonjs({
+            namedExports: {'react-is': [' isElement', 'isValidElementType', 'ForwardRef']}
+        }),
         terser(),
         postcss({plugins: []})
     ]
-}
+};
 export default config
